833bc73a3b72ae0a62d1791bae390938eb818ddd
NikolasChorianopoulos <chorianopoulosnikolas@gmail.com>
2025-02-18 18:59:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,14 +11,60 @@
 #include "SPIFFS.h"
 #include <SD.h>
 
+MatrixPanel_I2S_DMA matrix;
+
+
 // WiFi Credentials
-const char* ssid = "YOUR WIFI SSID";
-const char* password = "YOUR WIFI PASSWORD";
+const char* ssid = "Your Wifi SSID";
+const char* password = "Your Wifi Password";
+
+
+
+void wifiSetup(){
+  WiFi.begin(ssid, password);
+  int timeout = 10;
+  while (WiFi.status() != WL_CONNECTED && timeout > 0) {
+  delay(500);
+  Serial.print(".");
+  timeout--;
+  }
+
+  if (WiFi.status() != WL_CONNECTED){
+    Serial.println("WiFi connection failed.");
+    matrix.fillScreen(matrix.color565(250, 250, 250));
+    uint16_t borderColor = matrix.color565(0, 0, 0);
+    matrix.drawRect(2, 2, 60, 60, borderColor); //(x, y ,h ,w color)
+
+    matrix.setCursor(21, 21);
+    matrix.setTextColor(matrix.color565(0, 0, 0));
+    matrix.setTextSize(1);
+    matrix.print("WiFi"); 
+    matrix.setCursor(17, 31); 
+    matrix.print("Error");
+    delay(1000000);
+  }
+  else {
+    Serial.println("WiFi connected.");
+    matrix.fillScreen(matrix.color565(250, 250, 250));
+    uint16_t borderColor = matrix.color565(0, 0, 0);
+    matrix.drawRect(2, 2, 60, 60, borderColor); //(x, y ,h ,w color)
+
+    matrix.setCursor(21, 20);
+    matrix.setTextColor(matrix.color565(0, 0, 0));
+    matrix.setTextSize(1);
+    matrix.print("WiFi"); 
+    matrix.setCursor(5, 31); 
+    matrix.print("Connected");
+   
+  }
+
+  
+}
 
 // Spotify credentials
-const char* client_id = "YOUR SPOTIFY CLIENT ID";
-const char* client_secret = "YOUR SPOTIFY CLIENT SECRET";
-String refresh_token = "YOUR REFRESH TOKEN"; 
+const char* client_id = "Your Client ID";
+const char* client_secret = "Your Client Secret";
+String refresh_token = "Your Refresh Token"; // Obtain this from initial authorization
 String access_token;
 
 // Spotify API URLs
@@ -50,102 +96,68 @@ void refreshAccessToken() {
     http.end();
 }
 
-// Function to get currently playing song's cover art
-// Add a global variable to track the last imageUrl
+
+//global variable to track the last imageUrl
 String lastImageUrl = "";
 String currentImageUrl;
 
-void getCurrentlyPlaying() {
-   
-    HTTPClient http;
-    http.begin(currently_playing_url);
-    http.addHeader("Authorization", "Bearer " + access_token);
 
-    int httpResponseCode = http.GET();
-    if (httpResponseCode == 200) {
-        String response = http.getString();
 
-        // Parse the JSON response
-        DynamicJsonDocument doc(16384); // Increased buffer size
-        DeserializationError error = deserializeJson(doc, response);
-        if (error) {
-            Serial.print("JSON Deserialization failed: ");
-            Serial.println(error.f_str());
-            return;
-        }
 
-        // Extract the URL of the smallest album cover
-        const char* imageUrl = doc["item"]["album"]["images"][2]["url"]; // Change the index if you want a larger image
-        if (imageUrl) {
-            String currentImageUrl = String(imageUrl);
-            if (currentImageUrl != lastImageUrl) {
-                lastImageUrl = currentImageUrl; // Update the last imageUrl
-                Serial.println("Album Cover URL: " + lastImageUrl);
-            }
-            // No action needed if URL hasn't changed
-        } else {
-            Serial.println("Image URL not found in the response.");
-        }
-    } else if (httpResponseCode == 401) {
-        Serial.println("Access token expired. Refreshing token...");
-        refreshAccessToken(); // Token expired, refresh it
-    } else {
-        Serial.println("Failed to get currently playing song. HTTP code: " + String(httpResponseCode));
-    }
-    http.end();
+void displaySetup() {
+  // Matrix Panel Object
+  HUB75_I2S_CFG mxconfig(
+    64, // Width
+    64, // Height
+    1  // Number of chained panels
+  );
+
+    mxconfig.clkphase = false;
+
+    //Pin Configuration, Initialize Matrix, Text Settings
+    mxconfig.gpio.r1 = 25;
+    mxconfig.gpio.g1 = 26;
+    mxconfig.gpio.b1 = 27;
+    mxconfig.gpio.r2 = 14;
+    mxconfig.gpio.g2 = 12;
+    mxconfig.gpio.b2 = 13;
+    mxconfig.gpio.a = 23;
+    mxconfig.gpio.b = 19;
+    mxconfig.gpio.c = 5;
+    mxconfig.gpio.d = 17;
+    mxconfig.gpio.e = 32;
+    mxconfig.gpio.lat = 4;
+    mxconfig.gpio.oe = 15;
+    mxconfig.gpio.clk = 16;
+  
+    
+    matrix.begin(mxconfig);
+    matrix.fillScreen(matrix.color565(0, 0, 0));
+    matrix.setBrightness(150);
+    
+
 }
-
-
-
-// Matrix Configuration
-#define PANEL_RES_X 64
-#define PANEL_RES_Y 64
-#define PANEL_CHAIN 1
-
-// Pin Config
-#define R1_PIN 25
-#define G1_PIN 26
-#define B1_PIN 27
-#define R2_PIN 14
-#define G2_PIN 12
-#define B2_PIN 13
-#define A_PIN 23
-#define B_PIN 19
-#define C_PIN 5
-#define D_PIN 17
-#define E_PIN 32
-#define LAT_PIN 4
-#define OE_PIN 15
-#define CLK_PIN 16
-
-// Matrix Panel Object
-HUB75_I2S_CFG mxconfig(
-  PANEL_RES_X, // Width
-  PANEL_RES_Y, // Height
-  PANEL_CHAIN  // Number of chained panels
-);
-MatrixPanel_I2S_DMA matrix;
 
 // Downloaded Image Buffer
 uint8_t* jpgBuffer = nullptr;
 size_t jpgSize = 0;
 
 // Function to Render Pixels
-// Function to Render Pixels
 bool tftOutput(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap) {
-  // Loop through the rectangular region and draw each pixel
   for (int16_t row = 0; row < h; row++) {
     for (int16_t col = 0; col < w; col++) {
       int16_t pixelX = x + col;
       int16_t pixelY = y + row;
-      if (pixelX >= 0 && pixelY >= 0 && pixelX < PANEL_RES_X && pixelY < PANEL_RES_Y) {
-        matrix.drawPixel(pixelX, pixelY, bitmap[row * w + col]);
+      if (pixelX >= 0 && pixelY >= 0 && pixelX < 64 && pixelY < 64) {
+        uint16_t color = bitmap[row * w + col];
+        
+
+        matrix.drawPixel(pixelX, pixelY, color);
       }
     }
   }
   return true;
 }
-
 
 // Download Image from URL
 bool downloadImage() {
@@ -171,8 +183,7 @@ bool downloadImage() {
       }
     }
 
-    Serial.printf("HTTP GET Response Code: %d\n", httpCode);
-    
+       
     http.end();
     return true;
   }
@@ -182,67 +193,102 @@ bool downloadImage() {
   return false;
 }
 
+//No Song Playing Image
+void noSongPlaying(){
+
+  matrix.fillScreen(matrix.color565(250, 250, 250));
+  uint16_t borderColor = matrix.color565(0, 0, 0);
+  matrix.drawRect(2, 2, 60, 60, borderColor); //(x, y ,h ,w color)
+
+  matrix.setCursor(11, 20);
+  matrix.setTextColor(matrix.color565(0, 0, 0));
+  matrix.setTextSize(1);
+  matrix.print("No Song"); 
+  matrix.setCursor(11, 31); 
+  matrix.print("Playing");
+}
+
+//get currently playing track json info -> img url
+bool getCurrentlyPlaying() {
+  HTTPClient http;
+  http.begin(currently_playing_url);
+  http.addHeader("Authorization", "Bearer " + access_token);
+
+  int httpResponseCode = http.GET();
+  if (httpResponseCode != 200) {
+      Serial.println("Failed to fetch song. Showing 'No Song Playing'.");
+      return false;  // API error 
+  }
+
+  String response = http.getString();
+  DynamicJsonDocument doc(2048); // Reduced buffer size to prevent memory issues
+  DeserializationError error = deserializeJson(doc, response);
+
+  if (error) {
+      Serial.println("JSON Parse Error. Showing 'No Song Playing'.");
+      return false;  // JSON issue → Show "No Song Playing"
+  }
+
+  //Check if song is playing
+  bool isPlaying = doc["is_playing"];
+  if (!isPlaying) {
+      Serial.println("Spotify is paused or no song is playing.");
+      return false;
+  }
+
+  // Extract album cover URL if available
+  const char* imageUrl = doc["item"]["album"]["images"][2]["url"];  
+  if (!imageUrl) {
+      Serial.println("No album cover found. Showing 'No Song Playing'.");
+      return false;
+  }
+
+  // Update album cover only if changed
+  String newImageUrl = String(imageUrl);
+  if (newImageUrl != lastImageUrl) {
+      lastImageUrl = newImageUrl;
+      Serial.printf("HTTP GET Response Code: %d\n", HTTP_CODE_OK);
+      Serial.println("New album cover found: " + lastImageUrl);
+  }
+  return true; 
+}
+
+
 void setup() {
   Serial.begin(115200);
 
+  //Initiate Display
+  displaySetup();
+
   // Connect to WiFi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("WiFi connected.");
-
-  // Initialize Matrix
-  mxconfig.gpio.r1 = R1_PIN;
-  mxconfig.gpio.g1 = G1_PIN;
-  mxconfig.gpio.b1 = B1_PIN;
-  mxconfig.gpio.r2 = R2_PIN;
-  mxconfig.gpio.g2 = G2_PIN;
-  mxconfig.gpio.b2 = B2_PIN;
-  mxconfig.gpio.a = A_PIN;
-  mxconfig.gpio.b = B_PIN;
-  mxconfig.gpio.c = C_PIN;
-  mxconfig.gpio.d = D_PIN;
-  mxconfig.gpio.e = E_PIN;
-  mxconfig.gpio.lat = LAT_PIN;
-  mxconfig.gpio.oe = OE_PIN;
-  mxconfig.gpio.clk = CLK_PIN;
-
-  matrix.begin(mxconfig);
-  matrix.fillScreen(matrix.color565(0, 0, 0));
+  wifiSetup();  
+  
+  
+  // Refresh the token initially
+  refreshAccessToken();  
 
   // Initialize JPEG Decoder
   TJpgDec.setCallback(tftOutput);
   TJpgDec.setSwapBytes(false);
-
-  // Refresh the token initially
-    refreshAccessToken();
-
-  
+    
 
 }
 
 void loop() {
-  
-  getCurrentlyPlaying();
-  
-  
-  // Download and Display Image
-  if (downloadImage()) {
-    TJpgDec.drawJpg(0, 0, jpgBuffer, jpgSize);
-    free(jpgBuffer);
+ 
+  bool songPlaying = getCurrentlyPlaying();
+
+  if (songPlaying) {
+      
+      if (downloadImage()) {
+          TJpgDec.drawJpg(0, 0, jpgBuffer, jpgSize);
+          free(jpgBuffer);
+      }
   } else {
-    Serial.println("Image display failed.");
+      noSongPlaying();
   }
 
-    
+  delay(1000);  
+ 
 
-    Serial.printf("Expected JPEG Size: %d\n", jpgSize);
-    if (jpgBuffer) {
-        Serial.println("JPEG buffer successfully allocated.");
-    } else {
-        Serial.println("Failed to allocate JPEG buffer!");
-    }
-  delay(0);
 }
